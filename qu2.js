4440c0861ad93e72d617a83e86bd6e165e5f2b7d
sriyaradhika <91793895+sriyaradhika@users.noreply.github.com>
2021-11-01 20:30:17 +0530

    Add files via upload

---
+++ qu2.js
@@ -0,0 +1,11 @@
+var i= 2;
+function addition(){
+console.log("Globally value of i:"+i);}
+   addition();
+	function ADD(){
+
+         j=3;
+	console.log(" Locally value of j: =" +j);
+     }  ADD();
+   k=j+i;
+ console.log("Addition of i and j is:"+k); 
---
+++ qu3.js
@@ -0,0 +1,12 @@
+var i;
+console.log("Here we declared i but not assigned any value!");
+console.log("i = "+i);
+var j=85;
+if(j==undefined)
+console.log("j is undefined");
+else
+console.log("value of j is: "+j +" and it is defined");
+console.log(k);
+if(k==notdefined){
+    console.log("k is notdefined");
+}
